bb988370550b2696906d5b5374c951abaad67c54
bee2o2oo <bobooo@my.smccd.edu>
2024-10-03 14:12:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-# Import python packages
+o# Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col 
 
@@ -62,5 +62,5 @@ if ingredients_list:
         
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
-    
+#st.text(fruityvice_response.json())
+ fv_df = st.dataframe(data=  fruityvice_response.json(),use_container_width=True   )
