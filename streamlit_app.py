1ddafef8ba2d1d87c00902b0bafe0817511012a1
bee2o2oo <bobooo@my.smccd.edu>
2024-10-03 14:45:13 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 #convert the snowpark df to a pandas df so we can use the LOC function
-pd_df = my_dataframe.to_pands()
-st,dataframe(pd_df)
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
